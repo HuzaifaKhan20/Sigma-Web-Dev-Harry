c8c5b08ce050d138e8e5a5a26840ceb6d37588b6
Muhammad Huzaifa Khan <57367781+HuzaifaKhan20@users.noreply.github.com>
2024-02-25 03:06:25 +0500

    Add files via upload

---
+++ JS/75_Callbacks_Promises/script.js
@@ -0,0 +1,15 @@
+console.log("XK is hacker");
+
+setTimeout(() => {
+    console.log("I am inside set time out");
+}, 2000);
+
+console.log("UK is hacker");
+
+const callback = (arg)=>{
+    console.log(arg);
+}
+
+const loadScript = (src, callback)=>{
+    let sc = d
+}
